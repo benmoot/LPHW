689ce1ae7a25de0ed4d6263b26b747e3661f3009
benmoot <email.bennett.here@gmail.com>
2017-06-20 01:21:45 -0500

    test

---
+++ ex2.py
@@ -0,0 +1,20 @@
+# A comment, this is so you can read your program later.
+# Anything after # is ignored by python.
+print "I could have code like this." # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out a piece of code:
+# print "This won't run."
+print "This will run."
+
+# if a # is put inside of a string it will not be ignored
+print "Hi # there."
+
+# at this point in the program it requires a hash in front of every comment
+'''
+this is a multi
+comment line
+'''
+# if ''' is in front of text it will not be ran by program
+
+
+print TEST
